414e9c8c1772bc2ff742c47cfeae03ffde898a38
Valeriya Nekrashevich <73027852+Merildew@users.noreply.github.com>
2022-03-27 21:19:07 +0300

    mend

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app = express();
 app.use(express.json());
 
 let arr = [{id:2, name: "ehhe"}, {id:3, name: "keje"}];
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req,res) => {
     res.json(arr);
 })
 
-app.listen("3001", () => {
+app.listen(PORT, () => {
   console.log("Server running on port 3001");
 });
